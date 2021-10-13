f45f1a973585508c91c3b3b9556905c58af8b804
Ali Hussain <ali.h@nylas.com@Alis-MacBook-Pro.local>
2021-10-13 15:32:16 -0400

    removing atom and updating bashrc

--- atom/compile-cache/coffee/f55c001218bab56807639b76ab1aba348a7517f1.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL2hvbWUvYWxpc2FsZWVtaC8uYXRvbS9pbml0LmNvZmZlZSIKICBdLAogICJuYW1lcyI6IFtdLAogICJtYXBwaW5ncyI6ICJBQUFBO0FBQUE7QUFBQSIKfQ==
-
-//# sourceURL=/home/alisaleemh/.atom/init.coffee
